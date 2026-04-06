db2c3d4e4c9ab9f3f7ff2e18b04264ab1e061b30
Juan Jaramillo <195936872+jjaramillo20@users.noreply.github.com>
2026-04-06 11:00:49 -0600

    refactor: clean up leftover comments

--- src/main/java/com/fintech/wallet/model/entity/Transaction.java
+++ src/main/java/com/fintech/wallet/model/entity/Transaction.java
@@ -1,10 +1,7 @@
 package com.fintech.wallet.model.entity;
 
 import jakarta.persistence.*;
-<<<<<<< HEAD
 import org.hibernate.Hibernate;
-=======
->>>>>>> origin/main
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
